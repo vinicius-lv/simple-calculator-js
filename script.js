d735e46be23b1cd38aad6144f605b52b039a55ea
Vinicius Vasconcelos <vinnie.vasconcelos@gmail.com>
2022-07-28 13:41:36 -0300

    feat: Funcionalidades para limpar o campo de resultados, insercao de operadores e operacoes aritmeticas

--- script.js
+++ script.js
@@ -2,3 +2,18 @@ function insertNum(buttonNumber) {
     let resultBox = window.document.querySelector('p#result')
     resultBox.innerHTML += buttonNumber
 }
+
+function insertOperator(buttonOperator){
+    let resultBox = window.document.querySelector('p#result')
+    resultBox.innerHTML += buttonOperator
+}
+
+function deleteAllNumbers(){
+    window.document.querySelector('p#result').innerHTML = ''
+}
+
+function confirmResult(){
+    let resultBox = window.document.querySelector('p#result')
+    resultBox.innerHTML = eval(resultBox.innerHTML)
+    
+}
