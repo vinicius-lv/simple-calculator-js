17f96962285d730fb97b32a0542454eb562a5eeb
Vinicius Vasconcelos <vinnie.vasconcelos@gmail.com>
2022-08-01 23:23:33 -0300

    feat: Funcionalidade backspace

--- js/script.js
+++ js/script.js
@@ -1,19 +1,23 @@
 function insertNum(buttonNumber) {
-    let resultBox = window.document.querySelector('p#result')
+    let resultBox = window.document.querySelector('p#calc-result')
     resultBox.innerHTML += buttonNumber
 }
 
-function insertOperator(buttonOperator){
-    let resultBox = window.document.querySelector('p#result')
+function insertOperator(buttonOperator) {
+    let resultBox = window.document.querySelector('p#calc-result')
     resultBox.innerHTML += buttonOperator
 }
 
-function deleteAllNumbers(){
-    window.document.querySelector('p#result').innerHTML = ''
+function deleteAllNumbers() {
+    window.document.querySelector('p#calc-result').innerHTML = ''
 }
 
-function confirmResult(){
-    let resultBox = window.document.querySelector('p#result')
+function backspace() {
+    let resultBox = window.document.querySelector('p#calc-result').innerHTML
+    window.document.querySelector('p#calc-result').innerHTML = resultBox.substring(0, resultBox.length - 1)
+}
+
+function confirmOperation() {
+    let resultBox = window.document.querySelector('p#calc-result')
     resultBox.innerHTML = eval(resultBox.innerHTML)
-    
 }
